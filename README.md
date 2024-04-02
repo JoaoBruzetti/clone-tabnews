136fb63432218a5c63ef53c1c58c55070a04449d
João Victor Araujo Bruzetti <50773970+JoaoBruzetti@users.noreply.github.com>
2024-04-02 01:13:34 +0000

    Adicionando prettier no projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementação do https://www.tabnews.com.br/ para treino e expansão de conhecimentos
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1> Feliz Aniversario Rosseigo dos Santos!!!</h1>
+function Home() {
+  return <h1> Feliz Aniversario Rosseigo dos Santos!!!</h1>;
 }
 
-export default Home
+export default Home;
