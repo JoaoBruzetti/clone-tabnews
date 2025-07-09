db855922589769d74164e193ee4e1f49517ce278
João Victor Araujo Bruzetti <50773970+JoaoBruzetti@users.noreply.github.com>
2025-07-09 03:20:09 +0000

    start config database

---
+++ infra/database.js
@@ -0,0 +1,17 @@
+import { Client } from "pg";
+async function query(queryObject) {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "local_password",
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
