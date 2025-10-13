0ed0fb0cce4a8ea0c32d3f4abd52fffe7b6ca3ba
João Victor Araujo Bruzetti <50773970+JoaoBruzetti@users.noreply.github.com>
2025-10-13 23:59:36 +0000

    Finalizando aula dia 18

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,3 @@
-test("GET to /api/v1/sttus should return 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   expect((await fetch("http:localhost:3000/api/v1/status")).status).toBe(200);
 });
