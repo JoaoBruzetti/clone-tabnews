858b6b44678090e30cc1e468bcbc16f3230ef4fa
João Victor Araujo Bruzetti <50773970+JoaoBruzetti@users.noreply.github.com>
2025-10-16 00:26:46 +0000

    add ` updateAt`, postgress: `version`  `max_connections` `connenctions`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Valor" });
+  response.status(200).json({
+    updated_at: new Date().toISOString(),
+    postgres: {
+      version: Number(
+        (await database.query("SHOW server_version;")).rows[0].server_version,
+      ),
+      max_connections: Number(
+        (await database.query("SHOW max_connections;")).rows[0].max_connections,
+      ),
+      connections: Number(
+        (
+          await database.query(
+            "SELECT COUNT(*) AS total_connections FROM pg_stat_activity;",
+          )
+        ).rows[0].total_connections,
+      ),
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,25 @@
 test("GET to /api/v1/status should return 200", async () => {
-  expect((await fetch("http:localhost:3000/api/v1/status")).status).toBe(200);
+  const response = await fetch("http:localhost:3000/api/v1/status");
+
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  expect(responseBody.updated_at).toEqual(
+    new Date(responseBody.updated_at).toISOString(),
+  );
+
+  expect(responseBody.postgres.version).toBeDefined();
+  expect(responseBody.postgres.max_connections).toBeDefined();
+  expect(responseBody.postgres.connections).toBeDefined();
+
+  expect(typeof responseBody.postgres.version).toBe("number");
+  expect(typeof responseBody.postgres.max_connections).toBe("number");
+  expect(typeof responseBody.postgres.connections).toBe("number");
+
+  expect(responseBody.postgres.max_connections).toBeGreaterThan(
+    responseBody.postgres.connections,
+  );
 });
